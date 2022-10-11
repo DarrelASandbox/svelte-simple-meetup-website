880d6bd958d8b0523db75dbc2a17acc9ae6fc20c
Darrel Ang <darrelaiscoding@gmail.com>
2022-10-11 08:08:52 +0800

    Dynamic components

--- src/SpecialElements/SpecialElements.svelte
+++ src/SpecialElements/SpecialElements.svelte
@@ -1,15 +1,27 @@
 <script>
   import { CartItem, Product } from './';
 
-  let showProduct = true;
+  let renderedComponent = { cmp: Product, title: 'Test Product', id: 'p1' };
 
-  const toggle = () => (showProduct = !showProduct);
+  const toggle = () => {
+    renderedComponent.cmp === Product
+      ? (renderedComponent = {
+          cmp: CartItem,
+          title: 'CardItem title from SpecialElement.svelte',
+          id: 'p2',
+        })
+      : (renderedComponent = {
+          cmp: Product,
+          title: 'Product title from SpecialElement.svelte',
+          id: 'p1',
+        });
+  };
 </script>
 
 <button on:click={toggle}>Toggle Display</button>
 
-{#if showProduct}
-  <Product title="Product title from SpecialElement.svelte" id="p1" />
-{:else}
-  <CartItem title="CartItem title from SpecialElement.svelte" id="p1" />
-{/if}
+<svelte:component
+  this={renderedComponent.cmp}
+  title={renderedComponent.title}
+  id={renderedComponent.id}
+/>
